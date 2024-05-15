4cf697a291aca77d3abc20d6efc2c61109cba429
yudimanage <yudimanage@gmail.com>
2024-05-15 19:53:44 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post("/", (req, res) => {
-    axios.post(process.env.DISCORD_URL as string, {
-        content: JSON.stringify(req.body),
-        embeds: null,
-        username: "Bot",
-        avatar_url:
-            "https://img.freepik.com/free-vector/simple-vibing-cat-square-meme_742173-4493.jpg",
-        attachments: [],
-    });
+    const { donator_name } = req.body;
+
+    if (!donator_name) {
+        const { email_supporter, supporter, amount, currency, message }: SociabuzzType = req.body;
+        axios.post(process.env.DISCORD_URL as string, {
+            content: `||${email_supporter}||`,
+            embeds: [
+                {
+                    title: `${currency}${amount} from ${supporter}`,
+                    description: message,
+                    color: "#76cc11",
+                },
+            ],
+            username: "SociaBuzz",
+            avatar_url:
+                "https://img.freepik.com/free-vector/simple-vibing-cat-square-meme_742173-4493.jpg",
+            attachments: [],
+        });
+    } else {
+        const { donator_email, donator_name, amount_raw, message }: SaweriaType = req.body;
+        axios.post(process.env.DISCORD_URL as string, {
+            content: `||${donator_email}||`,
+            embeds: [
+                {
+                    title: `IDR${amount_raw} from ${donator_name}`,
+                    description: message,
+                    color: "#faae2b",
+                },
+            ],
+            username: "Saweria",
+            avatar_url:
+                "https://img.freepik.com/free-vector/simple-vibing-cat-square-meme_742173-4493.jpg",
+            attachments: [],
+        });
+    }
 
     res.sendStatus(200);
 });
@@ -28,3 +55,30 @@ app.get("/", (req, res) => {
 app.listen(3000, () => console.log("Server is running!!"));
 
 export default app;
+
+type SociabuzzType = {
+    id: string;
+    amount: number;
+    currency: string;
+    amount_settled: number;
+    currency_settled: string;
+    media_type: string;
+    media_url: string;
+    supporter: string;
+    email_supporter: string;
+    message: string;
+    created_at: string;
+};
+
+type SaweriaType = {
+    version: string;
+    created_at: string;
+    id: string;
+    type: string;
+    amount_raw: number;
+    cut: number;
+    donator_name: string;
+    donator_email: string;
+    donator_is_user: false;
+    message: string;
+};
